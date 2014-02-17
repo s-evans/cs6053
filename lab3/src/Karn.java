7bde6571a52ded2290f1eccf676a960853b41e66
Chris Geiger <ChrisMGeiger@gmail.com>
2014-02-16 20:15:42 -0500

    Added javadoc and unit test for Karn encryption

--- lab3/src/Karn.java
+++ lab3/src/Karn.java
@@ -16,6 +16,12 @@ public class Karn {
     static SecureRandom sr = null;  // This is expensive.  We only need one
     static MessageDigest md = null; // This will be shared.
 
+    /**
+     * Initializes a new Karn encryption class
+     * Creates a random source of SecureRandom and defines a SHA message digest
+     *
+     * @param bi Key represented as a BigInteger
+     */
     public Karn(BigInteger bi) {
         if (sr == null) sr = new SecureRandom();
         key = bi.toByteArray();
@@ -36,7 +42,12 @@ public class Karn {
         }
     }
 
-    // Encrypt the string using the karn algorithm
+    /**
+     * Encrypt the string using the karn algorithm
+     *
+     * @param plaintext Plaintext string to be encrypted
+     * @return base32 encoded encrypted string with guard byte and padding
+     */
     public String encrypt(String plaintext) {
         byte[] plain_left, plain_right;
         byte[] ciph_left, ciph_right;
@@ -62,6 +73,7 @@ public class Karn {
 
         while (cursor < input.length) {
             // Copy the next slab into the left and right
+            // TODO: This could be optimized with array copies
             for (int i = 0; i < HALF_BLOCK_SIZE_BYTES; i++) {
                 plain_left[i] = input[cursor + i];
                 plain_right[i] = input[cursor + HALF_BLOCK_SIZE_BYTES + i];
@@ -93,7 +105,11 @@ public class Karn {
         return (bi_out.toString(RADIX_SIZE_BYTES));
     }
 
-    // Decrypt the ciphertext by running Karn in reverse
+    /**
+     * Decrypt the ciphertext by running Karn in reverse
+     * Takes a base32 encoded stream with guard byte and padding
+     * Outputs the decrypted plaintext
+     */
     public String decrypt(String ciphertext) {
         BigInteger bi;
         byte input[];
@@ -119,6 +135,7 @@ public class Karn {
         //Decryption is a mirror of the encryption code
         while (pos < input.length) {
             // Copy the next slab into the left and right
+            // TODO: This could be optimized with array copies
             for (int i = 0; i < HALF_BLOCK_SIZE_BYTES; i++) {
                 ciphLeft[i] = input[pos + i];
                 ciphRight[i] = input[pos + HALF_BLOCK_SIZE_BYTES + i];
@@ -149,7 +166,9 @@ public class Karn {
         return StripPadding(out.toByteArray());
     }
 
-    // Padding
+    /**
+     * Pads the plaintext string to a blocklength per the Karn specification
+     */
     private byte[] StringToBytes(String input) {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         byte scratch[];
@@ -170,17 +189,80 @@ public class Karn {
         return (buffer.toByteArray());
     }
 
-    // Strip the header off the byte array and return the string
+    /**
+     * Removes the padding from the decrypted plaintext and returns a string
+     */
     private String StripPadding(byte input[]) {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         int i = 0;
 
+        // TODO: This could maybe done faster. Padding should only be on the last block.
         while (input[i] != 0 && i < input.length) {
             buffer.write(input[i]);
             i++;
         }
 
         return (new String(buffer.toByteArray()));
+    }
+    
+    public static void main(String[] args) {
+    	// Validate that encryption and decryption are consistent
+    	String plaintext = null;
+    	String ciphertext = null;
+        String decrypted = null;
+    	Karn karn = null;
+    	BigInteger key;
+        String testDescr;
+        int i;
+
+        testDescr = "Static key and a short plaintext of \"test\"";
+        key = new BigInteger("123456789", 16);
+        plaintext = "test";
+        karn = new Karn(key);
+        ciphertext = karn.encrypt(plaintext);
+        decrypted = karn.decrypt(ciphertext);
+        if (plaintext.equals(decrypted)) {
+            System.out.printf("Passed: %s\n", testDescr);
+        } else {
+            System.out.printf("Failed: %s\n", testDescr);
+        }
+
+        testDescr = "Static key and a long plaintext of \"The quick brown fox jumped over the lazy dog. The five boxing wizards jump quickly\"";
+        key = new BigInteger("123456789", 16);
+        plaintext = "The quick brown fox jumped over the lazy dog. The five boxing wizards jump quickly";
+        karn = new Karn(key);
+        ciphertext = karn.encrypt(plaintext);
+        decrypted = karn.decrypt(ciphertext);
+        if (plaintext.equals(decrypted)) {
+            System.out.printf("Passed: %s\n", testDescr);
+        } else {
+            System.out.printf("Failed: %s\n", testDescr);
+        }
+
+        testDescr = "Static key with plaintext from 0 to 256 characters";
+        key = new BigInteger("1234567890123456789012345678901234567890", 16);
+        String origPlaintext = "";
+        for (i=0; i < 256; i++) {
+            origPlaintext = origPlaintext + ((char)('a' + i % 26));
+        }
+        karn = new Karn(key);
+
+        for (i = 1; i < 256; i++) {
+            plaintext = origPlaintext.substring(0, i);
+            ciphertext = karn.encrypt(plaintext);
+            decrypted = karn.decrypt(ciphertext);
+            if (!plaintext.equals(decrypted)) {
+                break;
+            }
+        }
+
+        if ( i == 256 ) {
+            System.out.printf("Passed: %s\n", testDescr);
+        } else {
+            System.out.printf("Failed: %s\n", testDescr);
+        }
+
+        System.out.println("Karn test finished");
     }
 }
 
